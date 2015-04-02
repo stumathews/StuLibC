fce1f32ec1fe63a247fb2305382f80591b9b9232
Stuart Mathews <stumathews@gmail.com>
2015-04-02 08:07:02 +0100

    update my tcp test apps

--- src/tests/standalone/listen.c
+++ src/tests/standalone/listen.c
@@ -25,33 +25,19 @@ int main( int argc, char **argv )
 	const int on = 1;
     fd_set readfds;
     FD_ZERO( &readfds);
+    FD_SET(s, &readfds);
     struct timeval timeout = {.tv_sec = 60, .tv_usec=0}; 
-    DBG("timeout.tv_sec = %d timeout,tv_upsec = %d", timeout.tv_sec, timeout.tv_usec);
-
 
 	INIT();
-    /*
-     int select(int nfds, fd_set *readfds, fd_set *writefds,
-     fd_set *exceptfds, struct timeval *timeout);
-     void FD_CLR(int fd, fd_set *set);
-     int  FD_ISSET(int fd, fd_set *set);
-     void FD_SET(int fd, fd_set *set);
-     void FD_ZERO(fd_set *set);
-     * */
 
     // get a socket, bound to this address thats configured to listen.
     // NB: This is always ever non-blocking 
-    LOG( "Going to listen on port 9000...");
     s = tcp_server("localhost","9000");
-    DBG( "got socket...");
+
 
 	do
 	{
-        DBG("peerlen");
 		peerlen = sizeof( peer );
-        DBG("FD_SET...");
-        FD_SET(s, &readfds);
-        DBG("doing a select");
         // wait/block on this listening socket...
         int res = select( s+1, &readfds, NULL, NULL, &timeout);
         if( res == 0 )
@@ -82,6 +68,7 @@ int main( int argc, char **argv )
 
 		if ( !isvalidsock( s1 ) )
 			error( 1, errno, "accept failed" );
+
         // do network functionality on this socket that now represents a connection with the peer (client) 
 		server( s1, &peer );
 		CLOSE( s1 );
