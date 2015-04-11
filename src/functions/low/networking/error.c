ecfcbf9bed7f61ef4d63504ed0e0baaf837db986
Stuart Mathews <stumathews@gmail.com>
2015-04-11 17:38:43 +0100

    remove program name in erro func

--- src/functions/low/networking/error.c
+++ src/functions/low/networking/error.c
@@ -1,14 +1,12 @@
 #include <etcp.h>
 #include <debugging.h>
 
-char *program_name;
 
 void netError( int status, int err, char *fmt, ... )
 {
 	va_list ap;
 
 	va_start( ap, fmt );
-	fprintf( stderr, "%s: ", program_name );
 	vfprintf( stderr, fmt, ap );
     DBG("%s", strerror(err));
 	va_end( ap );
