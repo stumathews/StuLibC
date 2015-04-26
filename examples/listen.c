829c494f32629c24e488a92143c392508d9be129
Stuart Mathews <stumathews@gmail.com>
2015-04-26 20:55:11 +0100

    indents

--- examples/listen.c
+++ examples/listen.c
@@ -9,12 +9,12 @@ static void server( SOCKET s, struct sockaddr_in *peerp )
     int rc =  netReadLine(s, buf,256);
     if( rc <= 0 )
     {
-	    printf("prinf:%s\n",buf);
-	    PRINT("PRINTReceived data:%s\n",buf);
+        printf("prinf:%s\n",buf);
+        PRINT("PRINTReceived data:%s\n",buf);
     }
     else
     {
-	    PRINT("problem with readline\n");
+        PRINT("problem with readline\n");
     }
 }
 static void setPortNumber(char* arg)
@@ -25,88 +25,91 @@ static void setPortNumber(char* arg)
 
 int main( int argc, char **argv )
 {
-	LIB_Init();
+    LIB_Init();
 
-	struct Argument* portNumber = CMD_CreateNewArgument("port","port <number>","Set the port that the server will listen on", true, true, setPortNumber);
-	CMD_AddArgument(portNumber);
+    struct Argument* portNumber = CMD_CreateNewArgument("port","port <number>","Set the port that the server will listen on", true, true, setPortNumber);
+    CMD_AddArgument(portNumber);
 
-	struct sockaddr_in local;
-	struct sockaddr_in peer;
-	char *hname;
-	char *sname;
-	int peerlen;
-	SOCKET s1;
-	SOCKET s;
-	const int on = 1;
-	fd_set readfds;
+    struct sockaddr_in local;
+    struct sockaddr_in peer;
+    char *hname;
+    char *sname;
+    int peerlen;
+    SOCKET s1;
+    SOCKET s;
+    fd_set readfds;
+    FD_ZERO( &readfds);
+    const int on = 1;
 
-	FD_ZERO( &readfds);
-	struct timeval timeout = {.tv_sec = 60, .tv_usec=0}; 
+    struct timeval timeout = {.tv_sec = 60, .tv_usec=0}; 
 
-	if( argc > 1 )
-	{
-		enum ParseResult result = CMD_Parse(argc,argv,true);
-		if( result != PARSE_SUCCESS )
-		{
-			PRINT("There was a problem parsing: %d \n", result);
-			return 1;
-		}
-	}
-	else
-	{
-		CMD_ShowUsages("listen <options>");
-		exit(0);
-	}
+    if( argc > 1 )
+    {
+        enum ParseResult result = CMD_Parse(argc,argv,true);
+        if( result != PARSE_SUCCESS )
+        {
+            PRINT("There was a problem parsing: %d \n", result);
+            return 1;
+        }
+    }
+    else
+    {
+        CMD_ShowUsages("listen <options>");
+        exit(0);
+    }
 
-	INIT();
+    INIT();
 
-	// get a socket, bound to this address thats configured to listen.
-	// NB: This is always ever non-blocking 
-	s = netTcpServer("localhost",port);
+    // get a socket, bound to this address thats configured to listen.
+    // NB: This is always ever non-blocking 
+    s = netTcpServer("localhost",port);
 
-	FD_SET(s, &readfds);
+    FD_SET(s, &readfds);
 
-	do
-	{
-		peerlen = sizeof( peer );
-		// wait/block on this listening socket...
-		int res = select( s+1, &readfds, NULL, NULL, &timeout);
-		if( res == 0 )
-		{
-			LOG( "timeout");
-			netError(1,errno,"timeout!");
-		}
-		else if( res == -1 )
-		{
-			LOG("Select error!");
-			netError(1,errno,"select error!!");
-		}
-		else
-		{
+    do
+    {
+        // wait/block on this listening socket...
+        int res = select( s+1, &readfds, NULL, NULL, &timeout);
+        // wait/block 
 
-			if( FD_ISSET(s,&readfds ))
-			{
-				DBG("Accpeting");
-				s1 = accept( s, ( struct sockaddr * )&peer, &peerlen );
-			}
-			else
-			{
-				DBG("not our socket. continuing");
-				continue;
-			}
-		}
-		DBG("done");
+        if( res == 0 )
+        {
+            LOG( "timeout");
+            netError(1,errno,"timeout!");
+        }
+        else if( res == -1 )
+        {
+            LOG("Select error!");
+            netError(1,errno,"select error!!");
+        }
+        else
+        {
+            peerlen = sizeof( peer );
 
-		if ( !isvalidsock( s1 ) )
-			netError( 1, errno, "accept failed" );
+            if( FD_ISSET(s,&readfds ))
+            {
+                DBG("Accpeting");
+                s1 = accept( s, ( struct sockaddr * )&peer, &peerlen );
 
-		PRINT("getting socket\n");
-		// do network functionality on this socket that now represents a connection with the peer (client) 
-		server( s1, &peer );
-		PRINT("Closing.\n");
-		CLOSE( s1 );
-	} while ( 1 );
+                if ( !isvalidsock( s1 ) )
+                    netError( 1, errno, "accept failed" );
 
-	LIB_Uninit();
-	EXIT( 0 );
+                PRINT("getting socket\n");
+                // do network functionality on this socket that now represents a connection with the peer (client) 
+                server( s1, &peer );
+                PRINT("Closing.\n");
+                CLOSE( s1 );
+            }
+            else
+            {
+                DBG("not our socket. continuing");
+                continue;
+            }
+        }
+        DBG("done");
+
+    } while ( 1 );
+
+    LIB_Uninit();
+    EXIT( 0 );
 }
