20adda67fd090ad4e2e6e4d466344658a705c061
Stuart Mathews <stumathews@gmail.com>
2015-10-24 19:54:25 +0100

    enhance tests

--- src/tests/list.c
+++ src/tests/list.c
@@ -149,66 +149,94 @@ void addTen( Node* node)
 void testLIST_ForEach()
 {
 	List list = {0};
+
 	LIST_Init(&list);
+
 	LIST_Add(&list,(void*)10);
 	LIST_Add(&list,(void*)10);
 	LIST_Add(&list,(void*)10);
 	LIST_Add(&list,(void*)10);
 	LIST_Add(&list,(void*)10);
+
 	LIST_ForEach(&list, addTen);
-	Node* one = LIST_Get(&list, 0);
-	Node* three = LIST_Get(&list, 3);
 
-	/* random pick:*/
-	assert( one->data == (void*)11);
-	assert( three->data == (void*)11);
+	assert( list.size == 5);
 
+	for( int i = 0; i < list.size;i++)
+	{
+		Node* got = LIST_Get(&list,i);
+		assert( got->data == (void*)19);
+	}
 }
 
 void testLIST_DeleteNode()
 {
 	List list = {0};
+
 	LIST_Init(&list);
 
 	LIST_Add(&list,(void*)1);
 	LIST_Add(&list,(void*)2);
+
 	Node* three = LIST_Add(&list,(void*)3);
 	Node* four = LIST_Add(&list,(void*)4);
 	Node* five = LIST_Add(&list,(void*)5);
+
 	assert( list.size == 5);
+
 	LIST_DeleteNode(&list, three);
+
+	assert( list.size == 4);
 
 	Node* res1 = LIST_Get(&list, 2);
 	assert( res1 == four );
+
 	Node* res2 = LIST_Get(&list, 3);
 	assert( res2 == five);
 	assert( list.size == 4);
+
+	assert( (int) *(int*)LIST_Get(&list,0) == 1 );
+	assert( (int) *(int*)LIST_Get(&list,1) == 2 );
+	assert( (int) *((int*)three->data) == 3 );
+	assert( (int) *((int*)four->data) == 4 );
+	assert( (int) *((int*)five->data) == 5 );
 
 }
 
 void testLIST_FindData()
 {
 	List list = {0};
+
 	LIST_Init(&list);
 	LIST_Push(&list, (void*)1);
 	LIST_Push(&list, (void*)2);
+
 	Node* three = LIST_Push(&list, (void*)3);
 	Node* result = LIST_FindData(&list, (void*)3);
+
 	assert( result == three);
-	assert( list.size == 2);
+	assert( list.size == 3);
 
 }
 
 void testLIST_Get()
 {
 	const char* strMmathews = "Mathews";
+
 	List list = {0};
 	LIST_Init(&list);
 	LIST_Push(&list, (void*)"Stuart");
 	LIST_Push(&list, (void*)strMmathews);
+
+	assert( list.size == 2);
+
 	Node* mathews = LIST_Get(&list,1);
+
+	assert( list.size == 2);
 	assert( STR_Equals((char*)mathews->data, (char*)strMmathews) == true );
+
 	LIST_Deallocate(&list);
+	assert( list.size == 0);
 }
 int main( int argc, char** argv )
 {
