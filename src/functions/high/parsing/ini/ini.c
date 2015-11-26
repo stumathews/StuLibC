56e484d161b78980a0fe7b3a8aeb4b5003ac112e
Stuart Mathews <stumathews@gmail.com>
2015-11-26 11:26:47 +0000

    ini_parse returns a status

--- src/functions/high/parsing/ini/ini.c
+++ src/functions/high/parsing/ini/ini.c
@@ -6,16 +6,38 @@
 #include <safetychecking.h>
 #include <errors.h>
 
-void INI_IniParse(const char* filename, List* settings)
+int INI_IniParse(const char* filename, List* settings)
 {
 	CHECK_STRING( filename, IS_NOT_EMPTY );
-
 	yyin = fopen( filename, "r" );
 	if(yyin == null){
 		ERR_Print("Could not open .ini file", true);
+		return -1;
 	}
 
 	iniscan(settings);
 
 	fclose(yyin);
+	return 0;
+}
+
+char* INI_GetSetting(List* settings, char* section, char* setting)
+{
+	// Scan each section
+	for( int i = 0; i < settings->size; i++){
+		struct KeyValuePair* section = (struct KeyValuePair*) LIST_Get(settings, i)->data;
+		if( STR_Equals(section->key, section) == true )
+		{
+			List* section_settings = (List*) section->value;
+			for( int j = 0; j < section_settings->size;j++)
+			{
+				struct KeyValuePair* current_setting = (struct KeyValuePair*) LIST_Get(section_settings, j)->data;
+				if( STR_Equals(setting, current_setting->key ) == true )
+				{
+					return (char*) current_setting->value;
+				}
+			}
+		}
+	}
+	return null;
 }
