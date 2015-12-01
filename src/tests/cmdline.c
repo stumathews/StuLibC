e79df538184464683c7d68354cdbe4c0a090abca
Stuart Mathews <stumathews@gmail.com>
2015-12-01 22:48:19 +0000

    why not use a constant

--- src/tests/cmdline.c
+++ src/tests/cmdline.c
@@ -115,7 +115,7 @@ DBG("just after Parse\n");
 
 void test_CMD_ShowUsages()
 {
-    CMD_ShowUsages("Usage: test", "stumathews@gmail.com", "test cmd app");
+    CMD_ShowUsages("Usage: test", PACKAGE_BUGREPORT , "test cmd app");
 }
 
 int main(int argc, char* argv[])
