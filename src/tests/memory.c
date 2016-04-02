bb9e3937b143b6730c8e8fb2b5cc6749b99e2d0c
Stuart Mathews <stumathews@gmail.com>
2016-04-02 23:58:32 +0100

    removing MEM_ func tests

--- src/tests/memory.c
+++ src/tests/memory.c
@@ -11,44 +11,10 @@ int number = 25;
 
 void test_Alloc()
 {
-  List* mem_addrs = LIST_GetInstance();
-
-  aIntPtr = MEM_Alloc(SIZEOFINT, mem_addrs);
-  assert( aIntPtr != NULL );
-  
-  // try and deallocate a buffer not tracked/created by MEM_Alloc
-  int* ptr = Alloc( sizeof(int), mem_addrs );
-  assert( MEM_DeAlloc(ptr,"ptr", mem_addrs) == true);
-
-  int* ptr1 = Alloc( sizeof(int), mem_addrs);
-  assert( MEM_DeAlloc(ptr1,"ptr1", mem_addrs) == true);
-  
-  int* ptr2 = Alloc( sizeof(int), mem_addrs );
-  assert( MEM_DeAlloc(ptr2,"ptr2", mem_addrs) == true);
-  
-  int* a = Alloc(sizeof(int), mem_addrs);
-  assert( MEM_DeAlloc( a, "a", mem_addrs) == true);
- 
-  int* b = alloc(SIZEOFINT,mem_addrs);
-  int* c = alloc(SIZEOFINT,mem_addrs);
-  // create 10 random buffers 
-  for( int i = 0; i < 10;i++)
-  { 
-      void* buffer = alloc( SIZEOFINT, mem_addrs);
-  }
-  assert(MEM_GetTrackedCount(mem_addrs) == 13);
-
-  MEM_DeAllocAll(mem_addrs);
-  LIST_Deallocate(mem_addrs);
 }
 
 void test_DeAlloc()
 {
-	List* mem_addrs = LIST_GetInstance();
-	int* aIntPtr = MEM_Alloc(SIZEOFINT, mem_addrs);
-
-	MEM_DeAlloc(aIntPtr,"aIntPtr",mem_addrs);
-	LIST_Deallocate(mem_addrs);
 }
   
 
