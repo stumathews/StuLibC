12f7c7ceb1fddc6d0ffc8cb3e7f05f4cdbd001d8
Stuart Mathews <stumathews@gmail.com>
2016-04-02 23:57:21 +0100

    remove redundant MEM_ func protos

--- src/includes/memory.h
+++ src/includes/memory.h
@@ -22,47 +22,8 @@ This is a narative on this part of the library
 #include <list.h>
 
 // Common memory related functions like allocating memory or filling memory and freeing it etc.
-#define alloc(a,b) MEM_Alloc((a),(b))
+LIBRARY_API bool MEM_DeAlloc(void* buffer);
+LIBRARY_API void* MEM_Alloc(size_t size);
+LIBRARY_API bool MEM_CheckAllocated(void* buffer);
 
-/** \brief Detect empty pointers
- *
- * \param buffer void* pointer to the pointer returned by malloc
- * \param buffer_name char* the identifier of this buffer (include purpose)
- * \param filename char* the filename that this pointer is used in.(use __file__)
- * \param line int the line that this occures on (use __LINE__)
- * \param mem_pool List* local list used to track/scope allocated memory
- * \return LIBRARY_API bool true if is allocated or false if not
- *
- */
-LIBRARY_API bool MEM_CheckAllocated( void* buffer, char* buffer_name, char* filename, int line, List* mem_addrs);
-/** \brief Calls free() on the provided buffer, provided the buffer can be freed.
- *
- * Decrements the memory pool by one if buffer is found
- * \param buffer void*
- * \param buffer_name char*
- * \param mem_pool List* local list used to track/scope allocated memory
- * \return LIBRARY_API void
- *
- */
-LIBRARY_API bool MEM_DeAlloc(void* buffer, char* buffer_name, List* mem_addrs);
-#define MEM_Alloc(a, b) Alloc(a,b)
-
-// Not a library function to be used externally only internall by LIB_Uninit()
-LIBRARY_API void MEM_DeAllocAll(List* mem_pool);
-
-
-/** \brief Allocate a size amount of memory
- *
- * \param size size_t
- * \param mem_pool List* local list to track/scope allocated memory
- * \return LIBRARY_API void*
- */
-LIBRARY_API void* MEM_Alloc(size_t size, List* mem_pool);
-
-/** \brief Gets the number of tracked buffers by the library.
- * \param mem_pool List* local list used to track/scope allocated memory
- * \return LIBRARY_API int, the count of the number of tracked buffers
- *
- */
-LIBRARY_API int MEM_GetTrackedCount(List* mem_pool);
 #endif
